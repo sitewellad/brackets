322924a3617ca79fc23e3e561feb00d2522a0f85
sitewellad <sitewellad@gmail.com>
2021-03-08 17:49:27 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let checkArr = [];
+    for (let i = 0; i < str.length; i++) {
+        if (str[0] == 1) {
+            return true;
+        }
+        if (
+            str[i] == "{" ||
+            str[i] == "(" ||
+            str[i] == "[" ||
+            (str[i] == "|" && checkArr[checkArr.length - 1] != "|")
+        ) {
+            checkArr.push(str[i]);
+        } else if (
+            (checkArr[checkArr.length - 1] == "{" && str[i] == "}") ||
+            (checkArr[checkArr.length - 1] == "(" && str[i] == ")") ||
+            (checkArr[checkArr.length - 1] == "[" && str[i] == "]") ||
+            (str[i] == "|" && checkArr[checkArr.length - 1] == "|")
+        ) {
+            checkArr.pop();
+        } else {
+            return false;
+        }
+    }
+    return checkArr[0] == undefined ? true : false;
+};
